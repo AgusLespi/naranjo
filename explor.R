6792efa76faf40b80a6bdc13194adb7a5bb6249f
AgusLespi <aguslespiaucq@gmail.com>
2018-12-06 15:47:28 +0100

    inicio script

---
+++ explor.R
@@ -0,0 +1,20 @@
+###############################################################
+#
+#
+#
+#
+#### Contents
+#
+# A.1) Descripción de la población de árboles
+# 1. Abundancia de spp (gen/fam) con vs sin epifitas.
+# 2. Composición de spp. (gen/fam) con vs. sin epipfitas.
+
+setwd("G:/MS_in_prep/naranjo")
+
+foro <- read.csv("forof.csv", sep=",", head=T)
+datos <- foro
+
+# A.1.1) Abundancia spp con vs sin epifitas
+
+
+
